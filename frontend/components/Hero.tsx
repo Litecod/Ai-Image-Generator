c87b1d927f5232f52bbabdf7b734da4c8df37cd9
rozzdev <lightsamuel10@gmail.com>
2025-04-11 15:29:05 +0100

    update

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className="absolute w-full px-[0.8rem] md:px-[2rem] lg:px-[3rem] py-[30%] md:py-[10%] text-center top-0 left-0 text-[#fff] bg-[#000000a6] h-full">
                 <div className=" w-full mx-auto max-w-[900px] ">
                     <h1 className="text-transparent bg-clip-text bg-gradient-to-b from-[#fff] to-[#00000038] text-[2rem] sm:text-[3rem] md:text-[4rem] font-semibold">3D Cartoon Generator</h1>
-                    <p className="mt-[1.5rem] md:mt-[0]">Ever wondered what you, your pet, or even your breakfast would look like as a cartoon? Now you can find out! Upload any photo and let our cartoonizer work its magic. It's time to add some fun and animation to your images!</p>
+                    <p className="mt-[1.5rem] md:mt-[0]">Ever wondered what you, your pet, or even your breakfast would look like as a cartoon? Now you can find out! Upload any photo and let our cartoonizer work its magic. It&#39;s time to add some fun and animation to your images!</p>
                     <button className="mx-auto w-[10rem] bg-gradient-to-r from-[#4802b1] to-[#7800f0] rounded-xl py-[0.5rem] mt-[1rem]">Get Started</button>
                 </div>
             </div>
