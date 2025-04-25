c86391ce5d13137a24ef5374baba4db677716d6c
rozzdev <lightsamuel10@gmail.com>
2025-04-25 23:59:01 +0100

    date

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -4,14 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["oaidalleapiprodscus.blob.core.windows.net"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/backend-api/:path*",
-        destination: "https://ai-image-generator-backend-two.vercel.app/api/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/backend-api/:path*",
+  //       destination: "https://ai-image-generator-backend-two.vercel.app/api/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
