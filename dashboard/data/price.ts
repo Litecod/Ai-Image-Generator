55d86826da47c44edc2dd49ad6eda532e4376b59
rozzdev <lightsamuel10@gmail.com>
2025-05-02 23:43:01 +0100

    update

--- dashboard/data/price.ts
+++ dashboard/data/price.ts
@@ -1,19 +1,19 @@
 const priceOne = [
-    { id: 1, name: "Basic", price: 11, description: "Essential tools for individual creators starting their AI journey",billed: "weekly", credit: 20, image: 20, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
-    { id: 2, name: "Standard", price: 25, description: "Enhanced features for creators seeking more flexibility and support",billed: "weekly", credit: 50, image: 50, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
-    { id: 3, name: "Profession", price: 50, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "weekly", credit: 100, image: 100, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
+    { id: 1, name: "Basic", price: 5.99, description: "Essential tools for individual creators starting their AI journey",billed: "weekly", credit: 20, image: 20, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
+    { id: 2, name: "Standard", price: 9.99, description: "Enhanced features for creators seeking more flexibility and support",billed: "weekly", credit: 50, image: 50, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
+    { id: 3, name: "Profession", price: 12.99, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "weekly", credit: 100, image: 100, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
 ]
 
 const priceTwo = [
-    { id: 1, name: "Basic", price: 20, description: "Essential tools for individual creators starting their AI journey",billed: "monthly", credit: 45, image: 45, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
-    { id: 2, name: "Standard", price: 30, description: "Enhanced features for creators seeking more flexibility and support",billed: "monthly", credit: 65, image: 65, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
-    { id: 3, name: "Profession", price: 60, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "monthly", credit: 120, image: 120, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
+    { id: 1, name: "Basic", price: 9.99, description: "Essential tools for individual creators starting their AI journey",billed: "monthly", credit: 45, image: 45, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
+    { id: 2, name: "Standard", price: 19.99, description: "Enhanced features for creators seeking more flexibility and support",billed: "monthly", credit: 65, image: 65, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
+    { id: 3, name: "Profession", price: 24.99, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "monthly", credit: 120, image: 120, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
 ]
 
 const priceThree = [
-    { id: 1, name: "Basic", price: 20, description: "Essential tools for individual creators starting their AI journey",billed: "weekly", credit: 45, image: 45, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
-    { id: 2, name: "Standard", price: 35, description: "Enhanced features for creators seeking more flexibility and support",billed: "weekly", credit: 70, image: 70, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
-    { id: 3, name: "Profession", price: 70, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "weekly", credit: 150, image: 150, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
+    { id: 1, name: "Basic", price: 69.99, description: "Essential tools for individual creators starting their AI journey",billed: "weekly", credit: 45, image: 45, corcurrency: 4, style: "", piority: "", higher: "", period: "" },
+    { id: 2, name: "Standard", price: 99.99, description: "Enhanced features for creators seeking more flexibility and support",billed: "weekly", credit: 70, image: 70, corcurrency: 8, style: "", piority: "", higher: "", period: "" },
+    { id: 3, name: "Profession", price: 120, description: "Advanced tools and privacy for professionals maximizing productivity",billed: "weekly", credit: 150, image: 150, corcurrency: 12, style: "All styles and models", piority: "Priority Support", higher: "Higher priority in generation queue", period: "" },
 ]
 
 export {priceOne, priceTwo, priceThree}
